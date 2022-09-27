0011f02c439003ac0678642087b4799f0ca7318d
Mike Bott <mabott@gmail.com>
2022-09-27 10:21:33 -0700

    Cleanup of tests, test data, and config template

--- test_getrelatedobject.py
+++ test_getrelatedobject.py
@@ -8,7 +8,7 @@ import getrelatedobject
 
 class TestGetRelatedObject(unittest.TestCase):
     def test_get_object_type_from_event_shot(self):
-        shot_pickle_file = 'sghs_event_shot.pickle'
+        shot_pickle_file = 'test/sghs_event_shot.pickle'
         with open(shot_pickle_file, 'rb') as F:
             event = pickle.load(F)
         print(event)
@@ -16,7 +16,7 @@ class TestGetRelatedObject(unittest.TestCase):
         self.assertEqual(result, "Shot")
 
     def test_get_object_type_from_event_sequence(self):
-        sequence_pickle_file = 'sghs_event_sequence.pickle'
+        sequence_pickle_file = 'test/sghs_event_sequence.pickle'
         with open(sequence_pickle_file, 'rb') as F:
             event = pickle.load(F)
         print(event)
@@ -24,7 +24,7 @@ class TestGetRelatedObject(unittest.TestCase):
         self.assertEqual(result, "Sequence")
 
     def test_get_object_type_from_event_task(self):
-        task_pickle_file = 'sghs_event_task.pickle'
+        task_pickle_file = 'test/sghs_event_task.pickle'
         with open(task_pickle_file, 'rb') as F:
             event = pickle.load(F)
         print(event)
